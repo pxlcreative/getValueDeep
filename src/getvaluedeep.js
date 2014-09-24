cba7481f1bbd6f1eac91f41117778ed68821a9b3
Dan Cunningham <dcunningham@riskalyze.com>
2014-09-24 16:31:12 -0700

    Improved reliability of detecting missing paths (empty arrays and the like) Improved detection of invalid source objects

--- src/getvaluedeep.js
+++ src/getvaluedeep.js
@@ -1,24 +1,33 @@
 _.mixin({
-   getValueDeep: function(source,path,defaultVal,formatter) {
+   getValueDeep: function(source,path,defaultVal) {
            /*
+
+			Based on http://stackoverflow.com/questions/8817394/javascript-get-deep-value-from-object-by-passing-path-to-it-as-string
+            Extends to look at the path recursively and return default value if it can't get to the object it needs
 
             Get value of an object that may or may not be defined.
 
             - path = string of what object you are looking for i.e. "settings.config.value"
             - defaultVal = string of what you want to return if the obj returns nothing.
             - Returns empty string by default
-            - formatter = callback function to format the return value if it's not an empty string (i.e. if you wanted to add currency formatting to any non-blank value)
 
             */
 
            defaultVal = (typeof defaultVal == "undefined" ? '' : defaultVal);
 
-           if (typeof source == "undefined") return defaultVal; //kick out early if there is no source
+           //kick out early if there is no source set for some reason
+           if (
+               typeof source == "undefined"  // not set
+               || source == null             // null...
+               || typeof source !== "object" // has no properties
+               || source.length == 0         // empty object or array
+           ) return defaultVal;
 
            var value = recLookup(source,path);
+           
            function recLookup(obj, path) {
                var parts = path.split(".");
-               if ( parts.length==1 && obj[parts[0]] !== null){
+               if ( parts.length==1 && obj[parts[0]] !== null && typeof obj[parts[0]] !== "undefined"){
                    //we have reached our target value
                    return obj[parts[0]];
                } else if ( obj[parts[0]] == null || typeof obj[parts[0]] == "undefined" ) {
